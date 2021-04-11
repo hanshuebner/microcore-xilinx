7077421390fa1159c2486bca227a15f0b7889550
Hans Huebner <hans.huebner@gmail.com>
2021-04-11 10:10:34 +0200

    Papilio Pro configuration

--- Makefile
+++ Makefile
@@ -28,9 +28,11 @@ ifndef TARGET_PART
     $(error TARGET_PART must be defined)
 endif
 
+BUILD_DIR       ?= build/$(PROJECT)
+
 TOPLEVEL        ?= $(PROJECT)
 CONSTRAINTS     ?= $(PROJECT).ucf
-BITFILE         ?= build/$(PROJECT).bit
+BITFILE         ?= $(BUILD_DIR)/$(PROJECT).bit
 
 COMMON_OPTS     ?= -intstyle xflow
 XST_OPTS        ?=
@@ -57,6 +59,9 @@ AVS3A_EXE       ?= avs3a
 AVS3A_CABLE     ?= /dev/ttyACM1
 AVS3A_OPTS      ?= -s -b
 
+PAPILIO_PROG_EXE  ?= papilio-prog
+PAPILIO_PROG_OPTS ?= 
+
 
 ###########################################################################
 # Internal variables, platform-specific definitions, and macros
@@ -75,10 +80,10 @@ else
 endif
 
 TEST_NAMES = $(foreach file,$(VTEST) $(VHDTEST),$(basename $(file)))
-TEST_EXES = $(foreach test,$(TEST_NAMES),build/isim_$(test)$(EXE))
+TEST_EXES = $(foreach test,$(TEST_NAMES),$(BUILD_DIR)/isim_$(test)$(EXE))
 
 RUN = @/bin/echo -ne "\n\n\e[1;33m======== $(1) ========\e[m\n\n"; \
-	cd build && $(XILINX)/bin/$(XILINX_PLATFORM)/$(1)
+	cd $(BUILD_DIR) && $(XILINX)/bin/$(XILINX_PLATFORM)/$(1)
 
 # isim executables don't work without this
 export XILINX
@@ -91,24 +96,24 @@ export XILINX
 default: $(BITFILE)
 
 clean:
-	rm -rf build
+	rm -rf $(BUILD_DIR)
 
-build/$(PROJECT).prj: $(CONFIG)
+$(BUILD_DIR)/$(PROJECT).prj: $(CONFIG)
 	@echo "Updating $@"
-	@mkdir -p build
+	@mkdir -p $(BUILD_DIR)
 	@rm -f $@
-	@$(foreach file,$(VSOURCE),echo "verilog work \"../$(file)\"" >> $@;)
-	@$(foreach file,$(VHDSOURCE),echo "vhdl work \"../$(file)\"" >> $@;)
+	@$(foreach file,$(VSOURCE),echo "verilog work \"../../$(file)\"" >> $@;)
+	@$(foreach file,$(VHDSOURCE),echo "vhdl work \"../../$(file)\"" >> $@;)
 
-build/$(PROJECT)_sim.prj: build/$(PROJECT).prj
-	@cp build/$(PROJECT).prj $@
-	@$(foreach file,$(VTEST),echo "verilog work \"../$(file)\"" >> $@;)
-	@$(foreach file,$(VHDTEST),echo "vhdl work \"../$(file)\"" >> $@;)
+$(BUILD_DIR)/$(PROJECT)_sim.prj: $(BUILD_DIR)/$(PROJECT).prj
+	@cp $(BUILD_DIR)/$(PROJECT).prj $@
+	@$(foreach file,$(VTEST),echo "verilog work \"../../$(file)\"" >> $@;)
+	@$(foreach file,$(VHDTEST),echo "vhdl work \"../../$(file)\"" >> $@;)
 	@echo "verilog work $(XILINX)/verilog/src/glbl.v" >> $@
 
-build/$(PROJECT).scr: $(CONFIG)
+$(BUILD_DIR)/$(PROJECT).scr: $(CONFIG)
 	@echo "Updating $@"
-	@mkdir -p build
+	@mkdir -p $(BUILD_DIR)
 	@rm -f $@
 	@echo "run" \
 	    "-ifn $(PROJECT).prj" \
@@ -118,14 +123,14 @@ build/$(PROJECT).scr: $(CONFIG)
 	    "-top $(TOPLEVEL)" \
 	    "-ofmt NGC" \
 	    "-p $(TARGET_PART)" \
-	    > build/$(PROJECT).scr
+	    > $(BUILD_DIR)/$(PROJECT).scr
 
-$(BITFILE): $(CONFIG) $(VSOURCE) $(VHDSOURCE) $(CONSTRAINTS) build/$(PROJECT).prj build/$(PROJECT).scr
-	@mkdir -p build
+$(BITFILE): $(CONFIG) $(VSOURCE) $(VHDSOURCE) $(CONSTRAINTS) $(BUILD_DIR)/$(PROJECT).prj $(BUILD_DIR)/$(PROJECT).scr
+	@mkdir -p $(BUILD_DIR)
 	$(call RUN,xst) $(COMMON_OPTS) \
 	    -ifn $(PROJECT).scr
 	$(call RUN,ngdbuild) $(COMMON_OPTS) $(NGDBUILD_OPTS) \
-	    -p $(TARGET_PART) -uc ../$(CONSTRAINTS) \
+	    -p $(TARGET_PART) -uc ../../$(CONSTRAINTS) \
 	    $(PROJECT).ngc $(PROJECT).ngd
 	$(call RUN,map) $(COMMON_OPTS) $(MAP_OPTS) \
 	    -p $(TARGET_PART) \
@@ -147,21 +152,21 @@ trace: $(CONFIG) $(BITFILE)
 
 test: $(TEST_EXES)
 
-build/isim_%$(EXE): build/$(PROJECT)_sim.prj $(VSOURCE) $(VHDSOURCE) $(VTEST) $(VHDTEST)
+$(BUILD_DIR)/isim_%$(EXE): $(BUILD_DIR)/$(PROJECT)_sim.prj $(VSOURCE) $(VHDSOURCE) $(VTEST) $(VHDTEST)
 	$(call RUN,fuse) $(COMMON_OPTS) $(FUSE_OPTS) \
 	    -prj $(PROJECT)_sim.prj \
 	    -o isim_$*$(EXE) \
 	    work.$* work.glbl
 
-isim: build/isim_$(TB)$(EXE)
-	@grep --no-filename --no-messages 'ISIM:' $(TB).{v,vhd} | cut -d: -f2 > build/isim_$(TB).cmd
-	@echo "run all" >> build/isim_$(TB).cmd
-	cd build ; ./isim_$(TB)$(EXE) -tclbatch isim_$(TB).cmd
+isim: $(BUILD_DIR)/isim_$(TB)$(EXE)
+	@grep --no-filename --no-messages 'ISIM:' $(TB).{v,vhd} | cut -d: -f2 > $(BUILD_DIR)/isim_$(TB).cmd
+	@echo "run all" >> $(BUILD_DIR)/isim_$(TB).cmd
+	cd $(BUILD_DIR) ; ./isim_$(TB)$(EXE) -tclbatch isim_$(TB).cmd
 
-isimgui: build/isim_$(TB)$(EXE)
-	@grep --no-filename --no-messages 'ISIM:' $(TB).{v,vhd} | cut -d: -f2 > build/isim_$(TB).cmd
-	@echo "run all" >> build/isim_$(TB).cmd
-	cd build ; ./isim_$(TB)$(EXE) -gui -tclbatch isim_$(TB).cmd
+isimgui: $(BUILD_DIR)/isim_$(TB)$(EXE)
+	@grep --no-filename --no-messages 'ISIM:' $(TB).{v,vhd} | cut -d: -f2 > $(BUILD_DIR)/isim_$(TB).cmd
+	@echo "run all" >> $(BUILD_DIR)/isim_$(TB).cmd
+	cd $(BUILD_DIR) ; ./isim_$(TB)$(EXE) -gui -tclbatch isim_$(TB).cmd
 
 
 ###########################################################################
@@ -186,6 +191,11 @@ endif
 ifeq ($(PROGRAMMER), avs3a)
 prog: $(BITFILE)
 	$(AVS3A_EXE) -p $(AVS3A_CABLE) $(AVS3A_OPTS) $(BITFILE)
+endif
+
+ifeq ($(PROGRAMMER), papilio-prog)
+prog: $(BITFILE)
+	$(PAPILIO_PROG_EXE) $(PAPILIO_PROG_OPTS) -f $(BITFILE)
 endif
 
 ifeq ($(PROGRAMMER), none)
---
+++ vhdl/papilio-pro.vhd
@@ -0,0 +1,360 @@
+-- ---------------------------------------------------------------------
+-- ---------------------------------------------------------------------
+-- @file : papilio-pro.vhd - Toplevel for the Spartan-3A Evaluation Kit
+-- ---------------------------------------------------------------------
+--
+-- Last change: KS 02.04.2021 18:34:55
+-- @project: microCore
+-- @language: VHDL-93
+-- @copyright (c): Klaus Schleisiek, All Rights Reserved.
+-- @contributors:
+--
+-- @license: Do not use this file except in compliance with the License.
+-- You may obtain a copy of the Public License at
+-- https://github.com/microCore-VHDL/microCore/tree/master/documents
+-- Software distributed under the License is distributed on an "AS IS"
+-- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+-- See the License for the specific language governing rights and
+-- limitations under the License.
+--
+-- @brief: Top level microCore entity with umbilical debug interface.
+--         This file should be edited for technology specific additions
+--         like e.g. pad assignments and it is the source of the uBus.
+--
+-- Version Author   Date       Changes
+--   210     ks    8-Jun-2020  initial version
+--  2300     ks    8-Mar-2021  converted to NUMERIC_STD
+-- ---------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+USE work.functions_pkg.ALL;
+USE work.architecture_pkg.ALL;
+
+ENTITY fpga IS PORT (
+   clock        : IN  STD_LOGIC;        -- external clock input
+   led1         : OUT STD_LOGIC;
+-- external memory interface (unused on S3A-EVL)
+   ce_n        : OUT   STD_LOGIC;
+   oe_n        : OUT   STD_LOGIC;
+   we_n        : OUT   STD_LOGIC;
+   addr        : OUT   UNSIGNED(ram_addr_width-1 DOWNTO 0);
+   data        : INOUT UNSIGNED(ram_data_width-1 DOWNTO 0);
+-- umbilical uart for debugging
+   dsu_rxd     : IN    STD_LOGIC;  -- incoming asynchronous data stream
+   dsu_txd     : OUT   STD_LOGIC   -- outgoing data stream
+);
+
+ATTRIBUTE LOC       : STRING;
+ATTRIBUTE PULLMODE  : STRING;
+ATTRIBUTE IO_TYPE   : STRING;
+ATTRIBUTE DRIVE     : STRING;
+ATTRIBUTE SLEWRATE  : STRING;
+
+ATTRIBUTE LOC      OF clock       : SIGNAL IS "P94";
+   ATTRIBUTE PULLMODE OF clock    : SIGNAL IS "NONE";
+   ATTRIBUTE IO_TYPE  OF clock    : SIGNAL IS "LVTTL";
+
+ATTRIBUTE LOC      OF led1        : SIGNAL IS "P112";
+   ATTRIBUTE PULLMODE OF led1     : SIGNAL IS "NONE";
+   ATTRIBUTE IO_TYPE  OF led1     : SIGNAL IS "LVTTL";
+   ATTRIBUTE DRIVE    OF led1     : SIGNAL IS "8";
+   ATTRIBUTE SLEWRATE OF led1     : SIGNAL IS "SLOW";
+
+ATTRIBUTE LOC      OF dsu_rxd     : SIGNAL IS "P101";
+   ATTRIBUTE PULLMODE OF dsu_rxd  : SIGNAL IS "NONE";
+   ATTRIBUTE IO_TYPE  OF dsu_rxd  : SIGNAL IS "LVTTL";
+   ATTRIBUTE DRIVE    OF dsu_rxd  : SIGNAL IS "8";
+   ATTRIBUTE SLEWRATE OF dsu_rxd  : SIGNAL IS "FAST";
+
+ATTRIBUTE LOC      OF dsu_txd     : SIGNAL IS "P105";
+   ATTRIBUTE PULLMODE OF dsu_txd  : SIGNAL IS "UP";
+   ATTRIBUTE IO_TYPE  OF dsu_txd  : SIGNAL IS "LVTTL";
+   ATTRIBUTE DRIVE    OF dsu_txd  : SIGNAL IS "8";
+   ATTRIBUTE SLEWRATE OF dsu_txd  : SIGNAL IS "FAST";
+
+END fpga;
+
+ARCHITECTURE technology OF fpga IS
+
+SIGNAL uBus       : uBus_port;
+ALIAS  reset      : STD_LOGIC IS uBus.reset;
+ALIAS  clk        : STD_LOGIC IS uBus.clk;
+ALIAS  clk_en     : STD_LOGIC IS uBus.clk_en;
+
+SIGNAL reset_a    : STD_LOGIC; -- asynchronous reset positive logic
+SIGNAL reset_s    : STD_LOGIC; -- synchronized reset_n
+SIGNAL dsu_rxd_s  : STD_LOGIC;
+SIGNAL dsu_break  : STD_LOGIC;
+
+COMPONENT microcore PORT (
+   uBus        : IN    uBus_port;
+   core        : OUT   core_signals;
+   memory      : OUT   datamem_port;
+-- umbilical uart interface
+   rxd         : IN    STD_LOGIC;
+   break       : OUT   STD_LOGIC;
+   txd         : OUT   STD_LOGIC
+); END COMPONENT microcore;
+
+SIGNAL core         : core_signals;
+SIGNAL flags        : flag_bus;
+SIGNAL flags_pause  : STD_LOGIC;
+SIGNAL ctrl         : UNSIGNED(ctrl_width-1 DOWNTO 0);
+SIGNAL memory       : datamem_port; -- multiplexed memory signals
+
+-- data memory
+COMPONENT uDatacache PORT (
+   uBus        : IN  uBus_port;
+   rdata       : OUT data_bus;
+   dma_mem     : IN  datamem_port;
+   dma_rdata   : OUT data_bus
+); END COMPONENT uDatacache;
+
+SIGNAL dcache_rdata : data_bus;
+SIGNAL mem_rdata    : data_bus;
+SIGNAL dma_mem      : datamem_port;
+SIGNAL dma_rdata    : data_bus;
+SIGNAL cache_addr   : data_addr;    -- for simulation only
+
+-- external memory
+COMPONENT external_SRAM GENERIC (
+   ram_addr_width : NATURAL;
+   ram_data_width : NATURAL;
+   delay_cnt      : NATURAL    -- delay_cnt+1 extra clock cycles for each memory access
+); PORT (
+   uBus        : IN    uBus_port;
+   ext_rdata   : OUT   data_bus;
+   delay       : OUT   STD_LOGIC;
+-- external SRAM
+   ce_n        : OUT   STD_LOGIC;
+   oe_n        : OUT   STD_LOGIC;
+   we_n        : OUT   STD_LOGIC;
+   addr        : OUT   UNSIGNED(ram_addr_width-1 DOWNTO 0);
+   data        : INOUT UNSIGNED(ram_data_width-1 DOWNTO 0)
+); END COMPONENT external_SRAM;
+
+SIGNAL ext_rdata    : data_bus;
+SIGNAL SRAM_delay   : STD_LOGIC;
+
+-- board specific IO
+SIGNAL leds         : byte;
+SIGNAL time_int     : STD_LOGIC;
+SIGNAL ioreg        : UNSIGNED(14 DOWNTO 0);
+
+BEGIN
+
+flags(7 DOWNTO 5) <= (OTHERS => '0');
+
+-- ---------------------------------------------------------------------
+-- input signal synchronization
+-- ---------------------------------------------------------------------
+
+
+synch_reset: synchronize PORT MAP(clk, reset_a, reset_s);
+reset <= reset_a OR reset_s;
+
+synch_dsu_rxd:   synchronize   PORT MAP(clk, dsu_rxd, dsu_rxd_s);
+
+-- ---------------------------------------------------------------------
+-- clk generation (perhaps a PLL will be used)
+-- ---------------------------------------------------------------------
+
+clk <= clock;
+
+-- ---------------------------------------------------------------------
+-- ctrl-register (bitwise)
+-- ---------------------------------------------------------------------
+
+ctrl_proc: PROCESS (reset, clk)
+BEGIN
+   IF  reset = '1' AND ASYNC_RESET  THEN
+      ctrl <= (OTHERS => '0');
+   ELSIF  rising_edge(clk)  THEN
+      IF  uReg_write(uBus, CTRL_REG)  THEN
+         IF  uBus.wdata(signbit) = '0'  THEN
+               ctrl <= ctrl OR  uBus.wdata(ctrl'range);
+         ELSE  ctrl <= ctrl AND uBus.wdata(ctrl'range);
+         END IF;
+      END IF;
+      IF  reset = '1' AND NOT ASYNC_RESET  THEN
+         ctrl <= (OTHERS => '0');
+      END IF;
+   END IF;
+END PROCESS ctrl_proc;
+
+flags(f_bitout) <= ctrl(c_bitout);
+
+uBus.sources(CTRL_REG) <= resize(ctrl, data_width);
+
+-- ---------------------------------------------------------------------
+-- software semaphor f_sema using flag register
+-- ---------------------------------------------------------------------
+
+sema_proc : PROCESS (clk, reset)
+BEGIN
+   IF  reset = '1' AND ASYNC_RESET  THEN
+      flags(f_sema) <= '0';
+   ELSIF  rising_edge(clk)  THEN
+      IF  uReg_write(uBus, FLAG_REG)  THEN
+         IF  (uBus.wdata(signbit) XOR uBus.wdata(f_sema)) = '1'  THEN
+            flags(f_sema) <= uBus.wdata(f_sema);
+         END IF;
+      END IF;
+      IF  reset = '1' AND NOT ASYNC_RESET  THEN
+         flags(f_sema) <= '0';
+      END IF;
+   END IF;
+END PROCESS sema_proc;
+
+flags_pause <= '1' WHEN  uReg_write(uBus, FLAG_REG) AND uBus.wdata(signbit) = '0' AND
+                         unsigned(uBus.wdata(flag_width-1 DOWNTO 0) AND flags) /= 0
+               ELSE  '0';
+
+-- ---------------------------------------------------------------------
+-- microcore interface
+-- ---------------------------------------------------------------------
+
+flags(f_dsu) <= NOT dsu_break; -- '1' if debug terminal present
+
+uCore: microcore PORT MAP (
+   uBus       => uBus,
+   core       => core,
+   memory     => memory,
+-- umbilical uart interface
+   rxd        => dsu_rxd_s,
+   break      => dsu_break,
+   txd        => dsu_txd
+);
+
+-- control signals
+--ALIAS  reset        : STD_LOGIC IS uBus.reset;
+--ALIAS  clk          : STD_LOGIC IS uBus.clk;
+uBus.clk_en               <= core.clk_en;
+uBus.chain                <= core.chain;
+uBus.pause                <= flags_pause;
+uBus.delay                <= SRAM_delay;
+uBus.tick                 <= core.tick;
+-- registers
+uBus.sources(STATUS_REG)  <= resize(core.status, data_width);
+uBus.sources(DSP_REG)     <= resize(core.dsp, data_width);
+uBus.sources(RSP_REG)     <= addr_rstack_v(data_width-1 DOWNTO rsp_width) & core.rsp;
+uBus.sources(INT_REG)     <= resize(core.int, data_width);
+uBus.sources(FLAG_REG)    <= resize(flags, data_width);
+uBus.sources(VERSION_REG) <= to_unsigned(version, data_width);
+uBus.sources(DEBUG_REG)   <= core.debug;
+uBus.sources(TIME_REG)    <= core.time;
+-- data memory and return stack
+uBus.reg_en               <= core.reg_en;
+uBus.mem_en               <= core.mem_en;
+uBus.ext_en               <= core.ext_en;
+uBus.write                <= memory.write;
+uBus.addr                 <= memory.addr;
+uBus.wdata                <= memory.wdata;
+uBus.rdata                <= mem_rdata;
+
+-- ---------------------------------------------------------------------
+-- data memory consisting of dcache, ext_mem, and debugmem
+-- ---------------------------------------------------------------------
+
+dma_mem.enable <= '0';
+dma_mem.write  <= '0';
+dma_mem.addr   <= (OTHERS => '0');
+dma_mem.wdata  <= (OTHERS => '0');
+
+internal_data_mem: uDatacache PORT MAP (
+   uBus         => uBus,
+   rdata        => dcache_rdata,
+   dma_mem      => dma_mem,
+   dma_rdata    => dma_rdata
+);
+
+mem_rdata_proc : PROCESS (uBus, dcache_rdata, ext_rdata)
+BEGIN
+   mem_rdata <= dcache_rdata;
+   IF  uBus.ext_en = '1' AND WITH_EXTMEM  THEN
+      mem_rdata <= ext_rdata;
+   END IF;
+END PROCESS mem_rdata_proc;
+
+-- pragma translate_off
+memaddr_proc : PROCESS (clk)
+BEGIN
+   IF  rising_edge(clk)  THEN
+      IF  (clk_en AND core.mem_en) = '1'  THEN
+         cache_addr <= memory.addr; -- state of the internal blockRAM address register for simulation
+      END IF;
+END IF;
+END PROCESS memaddr_proc;
+-- pragma translate_on
+-- ---------------------------------------------------------------------
+-- external SRAM data memory
+-- ---------------------------------------------------------------------
+
+with_external_mem: IF  WITH_EXTMEM  GENERATE
+
+   SRAM: external_SRAM
+   GENERIC MAP (ram_addr_width, ram_data_width, 1)
+   PORT MAP (
+      uBus        => uBus,
+      ext_rdata   => ext_rdata,
+      delay       => SRAM_delay,
+   -- external SRAM
+      ce_n        => ce_n,
+      oe_n        => oe_n,
+      we_n        => we_n,
+      addr        => addr,
+      data        => data
+);
+
+END GENERATE with_external_mem; no_external_mem: IF  NOT WITH_EXTMEM  GENERATE
+
+   ext_rdata  <= (OTHERS => '0');
+   SRAM_delay <= '0';
+
+   ce_n <= '1';
+   we_n <= '1';
+   oe_n <= '1';
+   addr <= (OTHERS => '0');
+   data <= (OTHERS => 'Z');
+
+END GENERATE no_external_mem;
+
+-- ---------------------------------------------------------------------
+-- Spartan-3A Eval Kit specific IO
+-- ---------------------------------------------------------------------
+
+led_proc: PROCESS (reset, clk)
+BEGIN
+   IF  reset = '1' AND ASYNC_RESET  THEN
+      leds <= (OTHERS => '0');
+   ELSIF  rising_edge(clk)  THEN
+      IF  uReg_write(uBus, LED_REG)  THEN
+         leds <= uBus.wdata(7 DOWNTO 0);
+      END IF;
+      IF  reset = '1' AND NOT ASYNC_RESET  THEN
+         leds <= (OTHERS => '0');
+      END IF;
+   END IF;
+END PROCESS led_proc;
+
+uBus.sources(LED_REG) <= resize(leds, data_width);
+
+led1 <= NOT Ctrl(c_bitout) WHEN  SIMULATION  ELSE leds(0);
+
+time_int_proc : PROCESS (clk)
+BEGIN
+   IF  rising_edge(clk)  THEN
+      IF  uBus.tick = '1'  THEN
+         time_int <= '1';
+      END IF;
+      IF  uReg_write(uBus, FLAG_REG) AND uBus.wdata(signbit) = '1' AND uBus.wdata(i_time) = '0'  THEN
+         time_int <= '0';
+      END IF;
+   END IF;
+END PROCESS time_int_proc;
+
+flags(i_time) <= time_int;
+
+END technology;
+
